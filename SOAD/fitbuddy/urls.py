1e43c71f030f47bb5d7214652eaf1816adc4364f
HARICHANDANAAKULA <harichandana.a18@iiits.in>
2020-10-31 21:07:08 +0530

    homepage

--- SOAD/fitbuddy/urls.py
+++ SOAD/fitbuddy/urls.py
@@ -3,7 +3,9 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns=[
-    path('',views.index,name='index'),
+    path("", views.index_view, name="home"),
+
+    # path('',views.index,name='index'),
     path('register/',views.register,name='register'),
     path('customer_register/',views.customer_register.as_view(),name='customer_register'),
     path('fitness_register/',views.fitness_register.as_view(),name='fitness_register'),
--- SOAD/home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-def index_view(request):
-    return render(request, "home/home.html")
-def gallery_view(request):
-    return render(request, "home/gallery.html")
