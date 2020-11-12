c42720d8ec2920d3447024e1df264e56f874f34c
HaneeshaVakkalagadda <haneesha.v18@iiits.in>
2020-11-12 16:37:59 +0530

    added reviews to program api

--- SOAD/fitbuddy/serializers.py
+++ SOAD/fitbuddy/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Program, FitnessCenter
+from .models import Program, Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username')
+    program_name = serializers.CharField(source='program.title')
+    class Meta:
+        model = Review
+        fields=('id','program_name','username','comment','rating')
 
 class ProgramSerializer(serializers.ModelSerializer):
     fitnesscenter_name = serializers.CharField(source='fcenter.fitnesscenter_name')
+    reviews = ReviewSerializer(many=True, read_only=True)
     class Meta:
         model = Program
-        fields=('id','title','category','number_of_sessions','hours_per_session','price','description','image','fitnesscenter_name')
+        fields=('id','title','category','number_of_sessions','hours_per_session','price','description','image','fitnesscenter_name','reviews')
 
